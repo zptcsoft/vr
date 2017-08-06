b557ec86e2fc06c668396ee94e05022493d731f8
Louis de Wardt <louis@dewardt.co.uk>
2017-08-06 22:33:10 +0100

    Added first quick and dirty yaml tutorial file

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,11 @@ const autoprefixer = require('gulp-autoprefixer')
 const uglify = require('gulp-uglify')
 const connect = require('gulp-connect')
 
+const yaml = require('js-yaml')
+const fs = require('fs')
+
+const gulpHandlebars = require('gulp-handlebars-html')(require('handlebars'))
+
 gulp.task('sass', () => {
 	return gulp.src('./src/sass/*.scss')
 		.pipe(sass({ outputStyle: 'compressed' }))
@@ -22,12 +27,28 @@ gulp.task('js', () => {
 		.pipe(gulp.dest('./js'))
 })
 
-gulp.task('build', ['sass', 'js'])
+gulp.task('handlebars', () => {
+	try {
+  	var doc = yaml.safeLoad(fs.readFileSync('./src/tutorials/1-getting-started.yaml', 'utf8'));
+  	console.log(doc);
+	} catch (e) {
+  	console.log(e);
+	}
+
+	return gulp.src('./src/views/*.handlebars')
+		.pipe(gulpHandlebars({ tutorials: [doc] }, { }))
+		.pipe(rename('hello.html'))
+		.pipe(gulp.dest('./'));
+})
+
+gulp.task('build', ['sass', 'js', 'handlebars'])
 gulp.task('run', () => connect.server())
 
 gulp.task('watch', () => {
 	gulp.watch('./src/sass/*.scss', ['sass'])
 	gulp.watch('./src/js/*.js', ['js'])
+	gulp.watch('./src/views/*.handlebars', ['handlebars'])
+	gulp.watch('./src/tutorials/*.yaml', ['handlebars'])
 })
 
 gulp.task('default', ['build', 'run'])

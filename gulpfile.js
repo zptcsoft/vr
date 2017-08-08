2572a56909f7b1ae7aca42b6f30788e9a07d54fd
louisdewar <louis@dewardt.co.uk>
2017-08-08 16:00:03 +0100

    Tutorial handlebars (#1)
    All the tutorials are now specified in YAML files and handlebars generates static html from them
    * Added handlebars and it's gulp compiler
    
    * Added first quick and dirty yaml tutorial file
    
    * Tutorial data in YAML files
    
    * Handlebars working, now renders like original but from data
    
    * The neatening

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,12 @@ const autoprefixer = require('gulp-autoprefixer')
 const uglify = require('gulp-uglify')
 const connect = require('gulp-connect')
 
+const fs = require('fs')
+const glob = require('glob')
+
+const handlebars = require('gulp-hb')
+const yaml = require('js-yaml')
+
 gulp.task('sass', () => {
 	return gulp.src('./src/sass/*.scss')
 		.pipe(sass({ outputStyle: 'compressed' }))
@@ -22,12 +28,34 @@ gulp.task('js', () => {
 		.pipe(gulp.dest('./js'))
 })
 
-gulp.task('build', ['sass', 'js'])
+gulp.task('handlebars', callback => {
+	// Files are automatically sorted so the tutorials will be ordered correctly
+	glob('./src/tutorials/*.yaml', { }, function (err, files) {
+		if (err) console.error('Error with glob: ' + e)
+		tutorials = []
+
+		for (i in files) {
+			try { tutorials.push(yaml.safeLoad(fs.readFileSync(files[i], 'utf8'))) }
+			catch (e) { console.error('Error loading ' + files[i] + ': ' + e) }
+		}
+
+		gulp.src('./src/views/*.handlebars')
+			.pipe(handlebars({ data: { tutorials }, partials: './src/views/partials/*', preventIndent: true }))
+			.pipe(rename({ extname: '.html' }))
+			.pipe(gulp.dest('./'))
+			.on('end', callback)
+	})
+})
+
+gulp.task('build', ['sass', 'js', 'handlebars'])
 gulp.task('run', () => connect.server())
 
 gulp.task('watch', () => {
 	gulp.watch('./src/sass/*.scss', ['sass'])
 	gulp.watch('./src/js/*.js', ['js'])
+	gulp.watch(['./src/views/*.handlebars',
+		'./src/tutorials/*.yaml',
+		'./src/views/partials/*.handlebars'], ['handlebars'])
 })
 
 gulp.task('default', ['build', 'run'])
